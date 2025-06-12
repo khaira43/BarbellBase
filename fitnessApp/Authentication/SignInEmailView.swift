57a0ecea080b2bd9fe87bd5c9f7dbdc44010821f
khaira43 <rkhaira1010@gmail.com>
2025-06-12 11:39:36 -0400

    test

--- fitnessApp/Authentication/SignInEmailView.swift
+++ fitnessApp/Authentication/SignInEmailView.swift
@@ -7,12 +7,67 @@
 
 import SwiftUI
 
+@MainActor
+final class SignInEmailViewModel: ObservableObject {
+    
+    @Published var email = ""
+    @Published var password = ""
+    
+    func signIn() {
+        guard !email.isEmpty, !password.isEmpty else {
+            print("No email or password is found.")
+            return
+        }
+        Task {
+            do {
+                let returnedUserData = try await AuthenticationManager.shared.createUser(email: email, password: password)
+                print("Success")
+                print(returnedUserData)
+            } catch {
+                print("Error: \(error)")
+            }
+        }
+    }
+    
+}
+
 struct SignInEmailView: View {
+    @StateObject private var viewModel = SignInEmailViewModel()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            TextField("Email...", text: $viewModel.email)
+                .padding()
+                .background(Color.gray.opacity(0.4))
+                .cornerRadius(10)
+            
+            SecureField("Password...", text: $viewModel.password)
+                .padding()
+                .background(Color.gray.opacity(0.4))
+                .cornerRadius(10)
+            
+            Button {
+                viewModel.signIn()
+            } label: {
+                Text("Sign In")
+                    .font(.headline)
+                    .foregroundColor(.white)
+                    .frame(height: 55)
+                    .frame(maxWidth: .infinity)
+                    .background(Color.blue)
+                    .cornerRadius(10)
+            }
+            Spacer()
+        }
+        .padding()
+        .navigationTitle("Sign In With Email")
     }
 }
 
 #Preview {
-    SignInEmailView()
+    NavigationStack{
+        SignInEmailView()
+        
+    }
+    
 }
